19f70fc164433238f6071ae65a8a9caa782810d6
Sofia Puch <sofia.puch@aperto.com>
2021-12-08 18:37:47 +0100

    Add missing cards data to card tout list to make the full component

--- components/UI/organisms/cardList/CardToutList.js
+++ components/UI/organisms/cardList/CardToutList.js
@@ -7,31 +7,35 @@ const cards = [
     modifier: "primary",
     title: "Auto finden",
     description: "Ob Neuwagen oder Gebrauchtwagen, konfigurierbar oder ready to go - hier findest du dein neues Auto.",
-    link: {
-      label: "",
-      url: "",
-      icon: ""
-    }
+    link: {}
   },
   {
-    modifier: "light",
     title: "ALLES.AUTO Garage",
     description: "Registriere dich ganz einfach mit deinem aktuellen Fahrzeug für die Garage und sichere dir unzählige Vorteile.",
-    link: {
-      label: "",
-      url: "",
-      icon: ""
-    }
+    link: {}
+  },
+  {
+    modifier: "secondary",
+    title: "Ersatzteile & Zubehör",
+    description: "Egal was du suchst, hier findest du es! ",
+    link: {}
   },
   {
     modifier: "secondary",
     title: "Auto verkaufen",
     description: "Nutze den Preis-Check und finde heraus, wie viel du noch für deinen Alten bekommst.",
-    link: {
-      label: "",
-      url: "",
-      icon: ""
-    }
+    link: {}
+  },
+  {
+    modifier: "dark",
+    title: "Smarte Suche",
+    description: "Wir helfen dir, das perfekte Auto zu finden. Hier gehts zur Online-Beratung.",
+    link: {}
+  },
+  {
+    title: "Werkstatttermin buchen",
+    description: "Finde eine Werkstatt in deiner Nähe und bekomme Hilfe immer genau da, wo du sie brauchst.",
+    link: {}
   }
 ]
 
