e1edadd20d25ee54652d445c96702953fbc4992b
Sofia Puch <sofia.puch@aperto.com>
2021-12-13 16:47:51 +0100

    More fixes to be able to work with postcss only

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,7 @@
+/* Global style variables */
+import '../build/design-tokens/_tokens.css';
+
+/* Global styles */
 import '../styles/global.css';
 
 const App = ({ Component, pageProps }) => {
--- postcss.config.js
+++ postcss.config.js
@@ -1,19 +1,16 @@
 module.exports = {
-  plugins: [
-    'postcss-flexbugs-fixes',
-    'postcss-extend-rule',
-    'postcss-import',
-    [
-      'postcss-preset-env',
-      {
-        autoprefixer: {
-          flexbox: 'no-2009'
-        },
-        stage: 3,
-        features: {
-          'custom-properties': false
-        }
-      }
-    ]
-  ]
+  plugins: {
+    'postcss-flexbugs-fixes': {},
+    'postcss-preset-env': {
+      autoprefixer: {
+        flexbox: 'no-2009',
+      },
+      stage: 3,
+      features: {
+        'custom-properties': false,
+      },
+    },
+    'postcss-import': {},
+    'postcss-extend-rule': {}
+  },
 }
